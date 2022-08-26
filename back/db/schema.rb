f5297b2a091083871cd89cca9e86db43cc2cce8f
kaito071831 <kaito071831@gmail.com>
2022-08-26 17:10:28 +0900

    Board作成

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_140818) do
+ActiveRecord::Schema.define(version: 2022_08_26_075351) do
+
+  create_table "boards", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false
