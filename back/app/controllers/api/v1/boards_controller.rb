490f109ca6ba0d6911ea75a3387ea89e9f932dc4
kaito071831 <kaito071831@gmail.com>
2022-08-26 17:35:14 +0900

    アクション作成

--- back/app/controllers/api/v1/boards_controller.rb
+++ back/app/controllers/api/v1/boards_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::BoardsController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
+  def index
+    boards = Board.order(created_at: :desc)
+    render json: 
+      {
+        status: 'SUCCESS',
+        message: 'Loaded Boards',
+        data: boards
+      }
+  end
+
 end
