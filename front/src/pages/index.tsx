3f0d57293902992b2a668c6d6aca669b212fdadb
kaito071831 <kaito071831@gmail.com>
2022-08-28 05:22:59 +0900

    ログイン画面へのリンクを追加

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Sample</h1>
+      <Link href="/login">
+        <a>ログイン</a>
+      </Link>
     </div>
   )
 }
