78c5fbcce9d1c4af3f05c608968d1e961afa062c
kaito071831 <kaito071831@gmail.com>
2022-08-29 16:58:00 +0900

    型付けを行った

--- front/src/pages/boards/index.tsx
+++ front/src/pages/boards/index.tsx
@@ -1,16 +1,23 @@
 import { GetServerSideProps } from "next";
 import { Logout_button } from "../../components/logout_button";
 import { withAuthServerSideProps } from "../../libs/auth";
+import type { Boards, Board } from "../../types/board";
 
 export const getServerSideProps: GetServerSideProps = withAuthServerSideProps("api/v1/boards");
 
-const Boards = ({data}) => {
+type Props = {
+  data: Boards
+}
+
+const Boards = ({data}: Props) => {
+  const boardIndex: Board[] = data.boards;
+  console.log(boardIndex);
   return(
     <>
       Boards
       <Logout_button/>
-      {data.data.map((board, index) => (
-        <div id={index}>
+      {boardIndex.map((board: Board) => (
+        <div id={board.id.toString()}>
           <p>{board.id}</p>
           <p>{board.title}</p>
         </div>
