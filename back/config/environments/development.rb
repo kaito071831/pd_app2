1fa79a8c6b29438cad9d0fcf30c2491e8090d938
kaito071831 <kaito071831@gmail.com>
2022-08-30 03:33:36 +0900

    apiをホストに指定した通信を許可した

--- back/config/environments/development.rb
+++ back/config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # 他のdockerコンテナからの通信を許可する
+  config.hosts << "api"
 end
