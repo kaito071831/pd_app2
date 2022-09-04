7dc5dc933a1ce6550680c75d04f7c613471be389
kaito071831 <kaito071831@gmail.com>
2022-09-05 01:26:37 +0900

    メーラーの設定を行った

--- back/config/environments/development.rb
+++ back/config/environments/development.rb
@@ -66,4 +66,16 @@ Rails.application.configure do
   # 他のdockerコンテナからの通信を許可する
   config.hosts << "api"
 
+  config.action_mailer.default_options = { from: ENV['MAIL_USER'] }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['MAIL_USER'],
+    password: ENV['MAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
