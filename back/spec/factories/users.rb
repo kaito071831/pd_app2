b3af0750e8cfebfe8479702dccc3d94f5c84fcac
kaito071831 <kaito071831@gmail.com>
2022-09-05 01:34:06 +0900

    メール認証済みのユーザーを生成できるようにした

--- back/spec/factories/users.rb
+++ back/spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    confirmed_at { Date.current }
   end
 end
