9da353974a0800f96ef4ac056c3924c234be1931
kaito071831 <kaito071831@gmail.com>
2022-09-05 04:21:08 +0900

    サインアップページへのリンクを追加

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -7,7 +7,11 @@ const Home: NextPage = () => {
     <div>
       <h1>Sample</h1>
       <Link href="/sign_in">
-        <a>ログイン</a>
+        <a>サインイン</a>
+      </Link>
+      <br />
+      <Link href="/sign_up">
+        <a>サインアップ</a>
       </Link>
     </div>
   )
