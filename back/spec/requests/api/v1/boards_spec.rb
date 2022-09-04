b62ad22ae5007f174e0558a8dea091e836cae03b
kaito071831 <kaito071831@gmail.com>
2022-09-05 01:42:18 +0900

    ログイン無しのアクセスに対するテストを追加した

--- back/spec/requests/api/v1/boards_spec.rb
+++ back/spec/requests/api/v1/boards_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe "Api::V1::Boards", type: :request do
   let!(:token) { sign_in(user) }
 
   describe "GET /api/v1/boards" do
-    it "works! (now write some real specs)" do
+    it "ログインしたままアクセスできるか" do
       get api_v1_boards_path,  headers: token
       expect(response).to have_http_status(200)
+    end
+
+    it "ログインなしでアクセスできないようになっているか" do
+      get api_v1_boards_path
+      expect(response).to have_http_status(401)
     end
   end
 end
