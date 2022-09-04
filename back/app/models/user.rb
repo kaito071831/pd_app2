f926e756b37f1bedbaca6f7d53d205fd8428b28f
kaito071831 <kaito071831@gmail.com>
2022-09-05 07:09:17 +0900

    commentテーブルとの関連付けを定義

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :comments
 end
