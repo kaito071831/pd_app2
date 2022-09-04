5886d5dea804e70701847a0cf77c58c3da4e80b4
kaito071831 <kaito071831@gmail.com>
2022-09-05 05:00:58 +0900

    nullを拒否

--- back/db/migrate/20220829204608_create_boards.rb
+++ back/db/migrate/20220829204608_create_boards.rb
@@ -1,8 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[7.0]
   def change
     create_table :boards do |t|
-      t.string :title
-
+      t.string :title, null: false
       t.timestamps
     end
   end
