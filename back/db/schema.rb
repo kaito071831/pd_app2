91f8534a2f34541ec5929423a0c5d9d622e6af7c
kaito071831 <kaito071831@gmail.com>
2022-09-05 05:01:27 +0900

    nullを拒否

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_29_204608) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
