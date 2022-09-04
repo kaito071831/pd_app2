79e0415b16620b6114d040d6699821ab44233168
kaito071831 <kaito071831@gmail.com>
2022-09-05 03:05:39 +0900

    ユーザーを作成するようにした

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  user = User.new(email: "test@planet.kanazawa-it.ac.jp", password: "password")
+  user.skip_confirmation!
+  user.save!
+end
