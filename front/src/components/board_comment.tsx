2ce9eae4579be5c8ac984a64f5c987e9d6161c62
kaito071831 <kaito071831@gmail.com>
2022-10-02 23:46:22 +0900

    投稿内容をリアルタイムで反映できるようにした

--- front/src/components/board_comment.tsx
+++ front/src/components/board_comment.tsx
@@ -1,23 +1,72 @@
 import ActionCable from "actioncable"
+import { useState } from "react";
+import { getCookie } from "typescript-cookie";
+import { Comment } from "../types/comment";
 
-const BoardComment = (props) => {
-  const roomName: string = `Board${props.id}`
+type Props = {
+  id: number
+}
+
+type FormComment = {
+  name: string
+  comment: string
+  boardId: number
+  uid: string
+}
+
+const BoardComment = (props: Props) => {
+  const id: number = props.id;
+  const [comments, setComments] = useState<Comment[]>([])
+
+  const roomName: string = `Board${id}`
   const cable = ActionCable.createConsumer('ws://localhost:3000/cable');
   const boardChannel = cable.subscriptions.create(
     { channel: 'BoardChannel', room: roomName },
     {
-      received(response) {
-        console.log(response)
+      received(data) {
+        console.log(data.comments)
+        setComments(data.comments)
+      },
+
+      post: function(message){
+        this.perform('post', {comment: message})
       }
     }
   )
   const test = () => {
     boardChannel.perform('test', {})
   }
+
+  const createComment = (event: any) => {
+    const commentData = new FormData(event.currentTarget);
+    const comment: FormComment = {
+      name: commentData.get("name").toString(),
+      comment: commentData.get("comment").toString(),
+      boardId: id,
+      uid: getCookie('uid')
+    }
+    boardChannel.perform('post', {commentData: comment});
+    event.preventDefault();
+  }
+
   return(
     <>
-      aaaaaa
+      <div>
+        {comments.map((comment: Comment) => (
+          <div key={comment.id}>
+            <div>{comment.name}</div>
+            <div>{comment.comment}</div>
+          </div>
+        ))}
+      </div>
       <button onClick={test}>テスト</button>
+      <form onSubmit={createComment}>
+        <label>タイトル</label>
+        <input name="name" title="name" type="text"/><br/>
+        <label>本文</label><br/>
+        <textarea name="comment" title="comment" rows={4} cols={40} defaultValue=""/>
+        <button title="作成" type="submit">作成</button>
+      </form>
     </>
   )
 }
