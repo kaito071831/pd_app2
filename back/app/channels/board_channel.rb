b93fb4b48810c8df74182b2c952f730d01e55a77
kaito071831 <kaito071831@gmail.com>
2022-10-02 23:47:29 +0900

    フォームから投稿されたコメントをDBに登録できるようにした

--- back/app/channels/board_channel.rb
+++ back/app/channels/board_channel.rb
@@ -11,4 +11,12 @@ class BoardChannel < ApplicationCable::Channel
   def test
     ActionCable.server.broadcast("chat:#{params[:room]}", { body: "#{params[:room]}を購読する！"})
   end
+
+  def post(message)
+    boardId = message['commentData']['boardId']
+
+    Comment.create! name: message['commentData']['name'], comment: message['commentData']['comment'],
+      board_id: boardId, user_id: User.find_by(email: message['commentData']['uid']).id 
+    ActionCable.server.broadcast("board_channel_#{params[:room]}", {comments: Comment.where(board_id: boardId)})
+  end
 end
