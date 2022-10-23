ae83a1688db1556cae7e5cb769fd559ff25932a1
kaito071831 <kaito071831@gmail.com>
2022-10-23 20:32:19 +0900

    ページネーションを切り分け

---
+++ front/src/components/board_pagination.tsx
@@ -0,0 +1,38 @@
+import { AxiosInstance } from "axios";
+import { NextRouter, useRouter } from "next/router";
+import { createAxiosInstance } from "../libs/haveSession";
+import { Board } from "../types/board";
+import { Pagination } from "../types/pagination";
+
+type BoardPage = {
+  setBoardIndex: React.Dispatch<React.SetStateAction<Board[]>>
+  pagination: Pagination
+}
+
+export const BoardPagination = (props: BoardPage) => {
+  const setBoardIndex = (boards: Board[]) => {
+    props.setBoardIndex(boards);
+  }
+  const router: NextRouter = useRouter();
+  const pagination: Pagination = props.pagination;
+
+  const range = (start: number, end: number): number[] => [...Array(end - start + 1)].map((_, i) => start + i)
+  const movePage = (pageNum: number) => {
+    const axiosInstance: AxiosInstance = createAxiosInstance();
+    (async() => {
+      return await axiosInstance
+        .get(`boards/pagination?page=${pageNum}`)
+        .then((response) => {
+          setBoardIndex(response.data.data);
+        })
+    })();
+  }
+
+  return(
+    <>
+      {range(1, Math.ceil(pagination.total_count / pagination.limit_value)).map((num: number, index: number) => (
+        <span key={index} onClick={() => movePage(num)}>{num}</span>
+      ))}
+    </>
+  )
+}
