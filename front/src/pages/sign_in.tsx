4282c3ec0ab43e73e59ea0b5f457db934e4043f1
kaito071831 <kaito071831@gmail.com>
2022-11-01 05:04:56 +0900

    サインイン周りをスタイリング

--- front/src/pages/sign_in.tsx
+++ front/src/pages/sign_in.tsx
@@ -4,11 +4,40 @@ import axios, { AxiosInstance, AxiosResponse } from "axios"
 import { useState } from "react";
 import { removeCookie, setCookie } from "typescript-cookie";
 import { createAxiosInstance } from "../libs/haveSession";
+import { Alert, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Snackbar, TextField, Typography } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface State {
+  password: string;
+  showPassword: boolean;
+}
+
 
 const Sign_in: NextPage = () => {
   const router: NextRouter = useRouter();
   const [isError, setIsError] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [values, setValues] = useState<State>({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const signData: FormData = new FormData(event.currentTarget);
@@ -38,18 +67,43 @@ const Sign_in: NextPage = () => {
   }
   return(
     <>
-      <div>
-        {isError ? (
-          <p>{errorMessage}</p>
-        ) : null}
+      <Snackbar open={isError} autoHideDuration={60}>
+        <Alert severity="error" onClose={() => { setIsError(false) }}>{errorMessage}</Alert>
+      </Snackbar>
+      <Container>
+        <Typography align="center" variant="h3">サインイン</Typography>
         <form onSubmit={handleSubmit}>
-          <label>メールアドレス</label>
-          <input name="email" title="メールアドレス" autoComplete="email" autoFocus/>
-          <label>パスワード</label>
-          <input name="password" title="パスワード" type="password" autoComplete="current-password"/>
-          <button type="submit" title="ログイン">ログイン</button>
+          <FormControl variant="outlined" margin="dense" sx={{width: '100%'}}>
+            <TextField name="email" size="small" label="メールアドレス" autoComplete="email" autoFocus/>
+          </FormControl>
+          <FormControl variant="outlined" margin="dense" sx={{width: '100%'}}>
+            <InputLabel htmlFor="outlined-adornment-password" size="small">パスワード</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              size="small"
+              name="password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+            {/* <TextField name="password" size="small" label="パスワード" type="password" autoComplete="current-password"/> */}
+            <Button variant="contained" type="submit" title="ログイン" sx={{margin: 2}}>ログイン</Button>
+          </FormControl>
         </form>
-      </div>
+      </Container>
     </>
   )
 }
