89826fbeac9afbc52013e02dc5350a99fbf35b29
kaito071831 <kaito071831@gmail.com>
2022-11-15 13:22:43 +0900

    ヘッダーの作成

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,15 +1,42 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
-import { Logout_button } from "./logout_button"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Link from "next/link"
+import { useState } from "react"
+import { createAxiosInstance } from "../libs/haveSession"
+import { Signin_button } from "./Signin_button"
+import { Signout_button } from "./Signout_button"
 
 export const Header = () => {
+  const [isSession, setIsSession] = useState<boolean>(false);
+  
+    (async() => {
+      const axiosInstance = createAxiosInstance()
+      try{
+        await axiosInstance.get("auth/validate_token")
+          .then((res) => {
+            if(res.data.success == true){
+              setIsSession(true)
+            }else{
+              setIsSession(false)
+            }
+          })
+        }catch(e){
+          if(e.name !== "AxiosError"){
+            console.log(e);
+          }
+        }
+    })()
   return(
     <>
       <AppBar position="static">
         <Toolbar variant="regular">
-          <Typography component="div" sx={{flexGrow: 1}}>
-            Boards
-          </Typography>
-          <Logout_button/>
+          <Box component="div" sx={{flexGrow: 1}}>
+            <Link href={"/boards"}>
+              Boards
+            </Link>
+          </Box>
+          {isSession ? <Signout_button/> : <Signin_button/>}
         </Toolbar>
       </AppBar>
     </>
